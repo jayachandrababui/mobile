a6869c905f285448865ced79399e6be62555d8f9
root <root@ip-172-31-12-224.ap-south-1.compute.internal>
2021-03-20 13:22:46 +0000

    make changes

---
+++ file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+Only members with owner privileges for an organization or admin privileges for a repository can delete an organization repository. If Allow members to delete or transfer repositories for this organization has been disabled, only organization owners can delete organization repositories. For more information, see "Repository permission levels for an organization."
